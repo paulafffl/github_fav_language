502213f70d5739d3659d234675ddf6b693bea9f0
Paula Lima <paula.lima@192.168.1.128>
2020-08-16 18:01:51 +0100

    Refactored LoginBar to a stateless functional component

--- githublanguage/src/Components/LoginBar/LoginBar.js
+++ githublanguage/src/Components/LoginBar/LoginBar.js
@@ -1,16 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './LoginBar.css'
 
-class LoginBar extends Component {
-
-    render () {
-        return (
-            <div>
-                <p> First things first: log in to Github and it will grant access so we can find out your fav language</p>
-                <button className="LoginButton" onClick={this.props.onLogin} >LOGIN</button>
-            </div>
-        );
-    }
+const LoginBar = (props) => {
+    return (
+        <div>
+            <p> First things first: log in to Github and it will grant access so we can find out your fav language</p>
+            <button className="LoginButton" onClick={props.onLogin} >LOGIN</button>
+        </div>
+    );
 }
 
 export default LoginBar;
