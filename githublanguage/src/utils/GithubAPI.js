6ef178c762c2edf89e71c26a0b4e0a053b0c3d56
Paula Lima <paula.lima@192.168.1.128>
2020-08-16 15:20:44 +0100

    Refactored GithubAPI logic to separate the 3 requests into different functions

--- githublanguage/src/utils/GithubAPI.js
+++ githublanguage/src/utils/GithubAPI.js
@@ -2,49 +2,38 @@ const client_id = "6424af2a727bd1125847";
 const client_secret = "";
 const redirectUri = "http://localhost:3000/";
 
-let code;
 let accessToken;
 
 const GithubAPI = {
 
   mostFrequentItem(array) {
     return array.reduce((previous, current, i, arr) =>
-    arr.filter(item => item === previous).length >
-    arr.filter(item => item === current).length
-      ? previous
-      : current
-    );
+    arr.filter(item => item === previous).length > arr.filter(item => item === current).length ? previous : current);
   },
   
-  getCode(username){
-    // check for code
-    const codeMatched = window.location.href.match(/code=([^&]*)/);
-    if (codeMatched) {
-      code = codeMatched[1];
-      return code;
-    //1ST Makes a GET request to return code
-    } else {
-      localStorage.setItem('savedUsername', username);
-      const accessUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirectUri}`;
-      window.location = accessUrl;
-      console.log(".....1 and code is", code)
-    } 
+  getCode(){
+    //1ST Makes a GET request to retrieve a temporary code
+    const accessUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirectUri}`;
+    window.location = accessUrl;
   },
 
-  async search(username){
-    code = GithubAPI.getCode(username);
+  async getAccessToken(){
+    const code = GithubAPI.getCode();
     const codeUrl = `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}`;
     var proxyUrl = `https://cors-anywhere.herokuapp.com/${codeUrl}`;
     //2ND Makes a POST request to exchange code for access token
-    const response1 = await fetch(proxyUrl, {
+    const response = await fetch(proxyUrl, {
       headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
       method: 'POST',
     })
-    const jsonResponse1 = await response1.json();
-    const accessToken = jsonResponse1.access_token;
+    const jsonResponse = await response.json();
+    accessToken = jsonResponse.access_token;
+    return accessToken;
+  },
+
+  async search(username){
     //3RD Makes a GET request of all repos of the specified username
     const response = await fetch(`https://api.github.com/users/${username}/repos`, {
-        //headers: {Authorization: `token ${accessToken}`}
         headers: {Authorization: `${accessToken} OAUTH-TOKEN`}
       })
     const jsonResponse = await response.json();
